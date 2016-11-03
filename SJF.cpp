dc48ccd360855030f3fa35c79071ace8cc681266
Rishabh Gupta <rishabhg1997@gmail.com>
2016-11-03 14:44:07 +0530

    added sjf

--- SJF.cpp
+++ SJF.cpp
@@ -1,12 +1,42 @@
 #include "SJF.h"
 #include <queue>
-Result FCFS::calculate(std::vector<Process> processes)
+Result SJF::calculate(std::vector<Process> processes)
 {
     float atat = 0, awt = 0;
     //The vector of processes is already sorted in increasing order of their arrival time
     int n = processes.size();
+    int processesLeft = n;
     int time = 0;
+
     std::priority_queue<Process> readyQueue;
+    while(processesLeft > 0)
+    {
+        std::vector<Process>::iterator it = processes.begin();
+        while(it != processes.end())
+        {
+            if((*it).arrivalTime <= time)
+            {
+                readyQueue.push(*it);
+                it = processes.erase(it);
+            }
+            else
+            {
+                break;
+            }
+        }
+        if(readyQueue.empty())
+        {
+            time++;
+            continue;
+        }
+        Process current = readyQueue.top();
+        readyQueue.pop();
+        awt += time - current.arrivalTime;
+        do{
+            time++;
+        }while(!current.executeOneUnit());
+        atat += time - current.arrivalTime;
+    }
     Result result;
     result.avgTurnAroundTime = atat/n;
     result.avgWaitingTime = awt/n;
